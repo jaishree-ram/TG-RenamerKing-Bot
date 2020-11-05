1dbf1308a66fc5de1c4a51c4f331fadb86797eae
Jai Shree ram <73603048+jaishree-ram@users.noreply.github.com>
2020-11-05 22:18:58 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -84,7 +84,7 @@ async def generate_custom_thumbnail(bot, update):
         )
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["setthumbnail@Renamefiles28_bot"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["savephoto@Renamefiles28_bot"]))
 async def save_photo(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
